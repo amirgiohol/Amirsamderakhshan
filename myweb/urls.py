26465bd1a44648051d5029f9db6deadda5e99e80
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-20 01:50:36 +0330

    add data module

--- myweb/urls.py
+++ myweb/urls.py
@@ -2,14 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-
-def home(request):
-    return HttpResponse("OK")
-
-
 urlpatterns = [
-    path("", home),  # Healthcheck قطعی
     path("admin/", admin.site.urls),
-    path("profile/", include("app.profaile.urls")),
+    path("", include("app.profaile.urls")),
     path("contact/", include("app.contact.urls")),
 ]
+
