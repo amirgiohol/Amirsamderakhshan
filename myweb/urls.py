f57449360b8a4a3f0208ef2d49f8f94f204dec03
AmirsamDerakhshan <amirsamderakhshan@gmail.com>
2025-12-23 01:59:44 +0330

    add data module

--- myweb/urls.py
+++ myweb/urls.py
@@ -5,10 +5,15 @@ from django.http import HttpResponse
 # ویوی تستی
 def health_check(request):
     return HttpResponse("✅ سایت کار میکند!", content_type="text/plain; charset=utf-8")
+def health(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path('', health),                
     path("admin/", admin.site.urls),
     path("test/", health_check),  # اول این خط رو اضافه کن
     path("", include("app.profaile.urls")),
     path("contact/", include("app.contact.urls")),
+    
+    
 ]
